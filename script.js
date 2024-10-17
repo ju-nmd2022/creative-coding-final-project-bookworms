ced5a90611ef0e26fe1c84a05b3e9d6d14d540d7
ailimecool <emilia.fredriksson2003@gmail.com>
2024-10-17 13:17:09 +0200

    last change

--- script.js
+++ script.js
@@ -243,7 +243,7 @@ class Agent {
 
   draw() {
     push();
-    stroke(wind * random(5, 25), heatIndex * random(10, 30), temperature * random(3, 10), humidity + 70);
+    stroke(wind * random(5, 25), heatIndex * random(5, 10), temperature * random(3, 10), humidity + 70);
     strokeWeight(heatIndex);
     line(this.lastPosition.x, this.lastPosition.y, this.position.x, this.position.y);
     pop();
